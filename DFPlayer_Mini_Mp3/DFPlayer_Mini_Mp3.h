85ea6cacf3f3317b506fdc78805a1d04cce23269
leyap <leyapin@mail.com>
2014-05-26 15:35:07 +0800

    new
    first add

---
+++ DFPlayer_Mini_Mp3/DFPlayer_Mini_Mp3.h
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * DFPlayer_Mini_Mp3, This library provides a quite complete function for      * 
+ * DFPlayer mini mp3 module.                                                   *
+ * www.github.com/dfrobot/DFPlayer_Mini_Mp3 (github as default source provider)*
+ *  DFRobot-A great source for opensource hardware and robot.                  *
+ *                                                                             *
+ * This file is part of the DFplayer_Mini_Mp3 library.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is free software: you can redistribute it and/or          *
+ * modify it under the terms of the GNU Lesser General Public License as       *
+ * published by the Free Software Foundation, either version 3 of              *
+ * the License, or any later version.                                          *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with DFPlayer_Mini_Mp3. If not, see                           *
+ * <http://www.gnu.org/licenses/>.                                             *
+ *									       *
+ ******************************************************************************/
+
+/*
+ *	Copyright:	DFRobot
+ *	name:		DFPlayer_Mini_Mp3
+ *	version:	1.0
+ *	Author:		lisper <lisper.li@dfrobot.com>
+ *	Date:		2014-05-22
+ *	Description:	mp3 library for DFPlayer mini board
+ */
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+uint8_t send_buf[10] = {
+	0x7E, 0xFF, 06, 00, 00, 00, 00, 00, 00, 0xEF};
+uint8_t recv_buf[10];
+
+//* void(*send_func)() = NULL;
+//* HardwareSerial *hserial = NULL;
+//* SoftwareSerial *sserial = NULL;
+//* boolean is_reply = false;
+
+//7E FF 06 0F 00 01 01 xx xx EF
+//0-> 7E is start code
+//1-> FF is version
+//2-> 06 is length
+//3-> 0F is command
+//4-> 00 is no receive
+//5~6-> 01 01 is argument
+//7~8-> checksum = 0 - ( FF+06+0F+00+01+01 )
+//9-> EF is end code
+
+void mp3_set_reply (boolean state); 
+
+//
+void fill_uint16_bigend (uint8_t *thebuf, uint16_t data);
+
+//error because it is biggend mode in mp3 module
+//void fill_uint16 (uint8_t *thebuf, uint16_t data) {
+//        *(uint16_t*)(thebuf) = data;
+//}
+
+//
+void mp3_set_serial (HardwareSerial *theSerial); 
+
+//
+void mp3_set_serial (SoftwareSerial *theSerial); 
+
+//
+void h_send_func (); 
+
+//
+void s_send_func (); 
+
+//
+void mp3_send_cmd (); 
+
+//
+uint16_t get_mp3_checksum (uint8_t *thebuf); 
+
+//
+void mp3_fill_checksum (); 
+
+//
+void mp3_play (uint16_t num); 
+
+//
+void mp3_next (); 
+
+//
+void mp3_prev (); 
+
+//0x06 set volume 0-30
+void mp3_set_volume (uint16_t volume); 
+
+//0x07 set EQ0/1/2/3/4/5    Normal/Pop/Rock/Jazz/Classic/Bass
+void mp3_set_EQ (uint16_t eq); 
+
+//0x09 set device 1/2/3/4/5 U/SD/AUX/SLEEP/FLASH
+void mp3_set_device (uint16_t device); 
+
+//
+void mp3_sleep (); 
+
+//
+void mp3_reset (); 
+
+//
+void mp3_play (); 
+
+//
+void mp3_pause (); 
+
+//
+void mp3_stop (); 
+
+//
+void mp3_play_mp3 (uint16_t num); 
+
+//
+void mp3_get_state (); 
+
+//
+void mp3_get_volume (); 
+
+//
+void mp3_get_u_sum (); 
+
+//
+void mp3_get_tf_sum (); 
+
+//
+void mp3_get_flash_sum (); 
+
+//
+void mp3_get_tf_current (); 
+
+//
+void mp3_get_u_current (); 
+
+//
+void mp3_get_flash_current (); 
+
+//set single loop 
+void mp3_single_loop (boolean state); 
+
+//
+void mp3_DAC (boolean state); 
+
+//
+void mp3_random_play (); 
+
---
+++ DFPlayer_Mini_Mp3/examples/DFPlayer_Mini_Test/DFPlayer_Mini_Test.ino
@@ -0,0 +1,194 @@
+/*******************************************************************************
+ * DFPlayer_Mini_Mp3, This library provides a quite complete function for      * 
+ * DFPlayer mini mp3 module.                                                   *
+ * www.github.com/dfrobot/DFPlayer_Mini_Mp3 (github as default source provider)*
+ *  DFRobot-A great source for opensource hardware and robot.                  *
+ *                                                                             *
+ * This file is part of the DFplayer_Mini_Mp3 library.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is free software: you can redistribute it and/or          *
+ * modify it under the terms of the GNU Lesser General Public License as       *
+ * published by the Free Software Foundation, either version 3 of              *
+ * the License, or any later version.                                          *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with DFPlayer_Mini_Mp3. If not, see                           *
+ * <http://www.gnu.org/licenses/>.                                             *
+ *									       *
+ ******************************************************************************/
+
+/*
+ *	Copyright:	DFRobot
+ *	name:		DFPlayer_Mini_Mp3 test code
+ *	Author:		lisper <lisper.li@dfrobot.com>
+ *	Date:		2014-05-22
+ *	Description:	mp3 library for DFPlayer mini board
+ */
+ 
+ 
+#include <SoftwareSerial.h>
+
+#include "split.h"
+#include "read_serial.h"
+#include "DFPlayer_Mini_Mp3.h"
+
+#define BUFSIZE 20
+uint8_t buf[BUFSIZE];
+char *cmdbuf[8];
+
+//
+void setup () {
+        Serial1.begin (9600);
+        Serial.begin (9600);
+        while (!Serial);
+
+        mp3_set_serial (&Serial1);
+        mp3_set_volume (15);
+        mp3_get_tf_sum ();
+        print_info ();
+}
+
+void print_info () {
+        Serial.println ("you send:");
+        print_hex (send_buf, 10);
+        delay (100);
+        int recv_leng = read_serial_with_timeout (Serial1, recv_buf, 10, 3);
+        if (recv_leng) {
+                Serial.println ("you get:");
+                print_hex (recv_buf, 10);
+        }
+}
+
+//
+void loop () {        
+        int leng;
+        leng = read_serial_with_timeout (Serial1, buf, BUFSIZE, 3);
+        if (leng) {
+                Serial.print ("=>");
+                print_hex (buf, 10);
+        }
+        leng = read_serial_with_timeout (Serial, buf, BUFSIZE, 3);
+        if (leng) {
+                buf[leng] = '\0';
+                Serial.println ((char*)buf);
+                int cmdleng = split(cmdbuf, (char*)buf, 8);
+                if (cmdleng == 1) {
+                        if (strcmp (cmdbuf[0], "next") == 0) {
+                                mp3_next ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "prev") == 0) {
+                                mp3_prev ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "stop") == 0) {
+                                mp3_stop ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "next") == 0) {
+                                mp3_next ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "pause") == 0) {
+                                mp3_pause ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "state") == 0) {
+                                mp3_get_state ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "sum") == 0) {
+                                mp3_get_tf_sum ();
+                                print_info ();
+                        } 
+                        else if (strcmp (cmdbuf[0], "current") == 0) {
+                                mp3_get_tf_current ();
+                                print_info ();
+                        }
+                } 
+                else if (cmdleng == 2) {
+                        if (strcmp (cmdbuf[0], "play") == 0) {
+                                mp3_play (atoi (cmdbuf[1]));
+                                print_info ();
+                        }
+                        else if (strcmp (cmdbuf[0], "volume") == 0) {
+                                mp3_set_volume (atoi (cmdbuf[1]));
+                                print_info ();
+                        }
+                        else if (strcmp (cmdbuf[0], "reply") == 0) {
+                                if (strcmp (cmdbuf[1], "true") == 0)
+                                        mp3_set_reply (true);
+                                else if (strcmp (cmdbuf[1], "false") == 0)
+                                        mp3_set_reply (false);
+                                print_info ();
+                        }
+                        else if (strcmp (cmdbuf[0], "mp3") == 0) {
+                                mp3_play_mp3 (atoi (cmdbuf[1]));
+                                print_info ();
+                        }
+                        else if (strcmp (cmdbuf[0], "eq") == 0) {
+                                mp3_set_EQ (atoi (cmdbuf[1]));
+                                print_info ();
+                        }
+                        else if (strcmp (cmdbuf[0], "single") == 0) {
+                                if (strcmp (cmdbuf[1], "true") == 0)
+                                        mp3_single_loop (true);
+                                else if (strcmp (cmdbuf[1], "false") == 0)
+                                        mp3_single_loop (false);
+                                print_info ();
+                        }
+                }
+
+///////////////////////////////////////////////////////////
+//                if (cmdleng == 1) {
+//                        switch (cmdbuf[0][0]) {
+//                        case 't':
+//                                mp3_stop ();
+//                                break;
+//                        case 'n':
+//                                mp3_next ();
+//                                break;
+//                        case 'p':
+//                                mp3_prev ();
+//                                break;
+//                        case 's':
+//                                mp3_sleep ();
+//                                break;
+//                        case 'r':
+//                                mp3_reset ();
+//                                break;
+//                        }
+//                }
+//                else if (cmdleng == 2) {
+//                        switch (cmdbuf[0][0]) {
+//                        case 'p':
+//                                mp3_play (atoi (cmdbuf[1]));
+//                                break;
+//                        case 'v':
+//                                mp3_set_volume (atoi (cmdbuf[1]));
+//                                break;
+//                        case 'm':
+//                                mp3_play_mp3 (atoi (cmdbuf[1]));
+//                                break;
+//
+//                        }
+//                }
+//////////////////////////////////////////////////
+
+        }
+}
+
+
+
+
+
